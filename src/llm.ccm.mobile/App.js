f7e90b1bbf4c288cfec3c0e88dea101457a0d174
Arul Selvan <arulselvanj@gmail.com>
2018-10-21 15:46:59 +0530

    initial login page design

--- src/llm.ccm.mobile/App.js
+++ src/llm.ccm.mobile/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button , Alert } from 'react-native';
 
 export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { mobile: 'Enter Mobile Number', password:'Enter Password' };
+  }
+  
+  onSubmit = function(){
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to LLM CCM!</Text>
+        
+        <TextInput
+        style={{height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.mobile}/>  
+
+        <TextInput
+        style={{height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.password}/>   
+
+       <Button
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+          title="Submit"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+       
       </View>
     );
   }
