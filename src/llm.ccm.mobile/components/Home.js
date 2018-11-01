5691aed0c8e24265f208eb1c7a19526538686253
TURNER\c-hclent-aselvan <arulselvanj@gmail.com>
2018-11-01 17:30:16 +0530

    added header

--- src/llm.ccm.mobile/components/Home.js
+++ src/llm.ccm.mobile/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View , H1} from 'native-base';
+import {AppHeader} from './common/AppHeader'
 
 export class Home extends Component {
 
     render() {
         return (
             <View>
+                <AppHeader/>
                 <H1>Wlecome to LLM CCM!</H1>
             </View>
         )
